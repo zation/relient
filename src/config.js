e7080ebb25e16f9fbd0d385618121a0d36fa05d1
Liu Yang <liuyang@waijule.com>
2017-11-15 19:02:49 +0800

    improve config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,17 @@
-import { propertyOf, pick } from 'lodash/fp';
+import { propertyOf, pick, flow } from 'lodash/fp';
+import serialize from 'serialize-javascript';
 
-const config = {
-  DNS_DOMAIN: process.env.DNS_DOMAIN,
-  API_Domain: process.env.API_Domain,
-};
+if (!global.reboxesConfig) {
+  global.reboxesConfig = {
+    DNS_DOMAIN: process.env.DNS_DOMAIN,
+    API_DOMAIN: process.env.API_DOMAIN,
+    GOOGLE_TRACKING_ID: process.env.GOOGLE_TRACKING_ID,
+    PORT: process.env.PORT || 3000,
+  };
+}
+
+const config = global.reboxesConfig;
+const clientConfigs = ['DNS_DOMAIN', 'API_DOMAIN'];
 
 export const getConfig = propertyOf(config);
-export const getConfigs = names => (names ? pick(names)(config) : config);
+export const initClientConfig = `window.reboxesConfig=${flow(pick(clientConfigs), serialize)(config)}`;
