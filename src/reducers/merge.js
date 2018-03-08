d198a9d8642003a5f97fc39e67bf451d343ee9b1
Liu Yang <zation1@gmail.com>
2018-03-08 21:35:10 +0800

    fix merge

--- src/reducers/merge.js
+++ src/reducers/merge.js
@@ -1,16 +1,19 @@
-import { isArray, mapValues, prop, flow, identity, pick } from 'lodash/fp';
+import { isArray, mapValues, prop, flow, identity } from 'lodash/fp';
 import normalize from './normalize';
 
 const mapValuesWithKey = mapValues.convert({ cap: false });
 
-export default ({ schema, entityKey, dataKey, attributes }) => (originalData, { payload }) =>
-  flow(
-    dataKey ? prop(dataKey) : identity,
-    normalize(isArray(payload) ? [schema] : schema),
-    prop(`entities.${entityKey || schema.key}`),
-    mapValuesWithKey((value, key) => ({
-      ...originalData[key],
-      ...(attributes ? pick(attributes)(value) : value),
-    })),
-    newData => ({ ...originalData, ...newData }),
-  )(payload);
+export default ({ schema, entityKey, preProcess, dataKey, processValue, postProcess }) =>
+  (originalData, { payload, meta }) =>
+    flow(
+      dataKey ? prop(dataKey) : identity,
+      preProcess ? data => preProcess({ originalData, meta, payload, data }) : identity,
+      data => normalize(isArray(data) ? [schema] : schema)(data),
+      prop(`entities.${entityKey || schema.key}`),
+      mapValuesWithKey((value, key) => ({
+        ...originalData[key],
+        ...(processValue ? processValue({ meta, originalData, payload, value }) : value),
+      })),
+      newData => ({ ...originalData, ...newData }),
+      postProcess ? data => postProcess({ data, originalData, meta, payload }) : identity,
+    )(payload);
