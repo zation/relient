349f312c7097f729a59ca38e3976a0801a4e95f9
Liu Yang <liuyang@waijule.com>
2018-03-22 12:39:27 +0800

    pass getState for onGlobalWarning and onUnauthorized

--- src/middlewares/server-error.js
+++ src/middlewares/server-error.js
@@ -1,17 +1,18 @@
 import { THROW_SERVER_ERROR } from '../actions/server-error';
 
-export default ({ onGlobalWarning, onUnauthorized } = {}) => ({ dispatch }) => next => (action) => {
-  if (action.type === THROW_SERVER_ERROR) {
-    const {
-      meta: { status, ignoreGlobalWarning, ignoreAuthRedirection },
-      payload,
-    } = action;
-    if (!ignoreGlobalWarning && onGlobalWarning) {
-      onGlobalWarning({ payload, dispatch });
+export default ({ onGlobalWarning, onUnauthorized } = {}) =>
+  ({ dispatch, getState }) => next => (action) => {
+    if (action.type === THROW_SERVER_ERROR) {
+      const {
+        meta: { status, ignoreGlobalWarning, ignoreAuthRedirection },
+        payload,
+      } = action;
+      if (!ignoreGlobalWarning && onGlobalWarning) {
+        onGlobalWarning({ payload, dispatch, getState });
+      }
+      if (status === 401 && !ignoreAuthRedirection && onUnauthorized) {
+        onUnauthorized({ payload, dispatch, getState });
+      }
     }
-    if (status === 401 && !ignoreAuthRedirection && onUnauthorized) {
-      onUnauthorized({ payload, dispatch });
-    }
-  }
-  return next(action);
-};
+    return next(action);
+  };
