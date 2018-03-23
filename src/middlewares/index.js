5072c14a9c043737fabd51cc25037d5cf11c5bfe
Liu Yang <liuyang@waijule.com>
2018-03-23 14:10:37 +0800

    remove client authorization

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,3 @@
-export { default as clientAuthorization } from './client-authorization';
 export { default as fetch } from './fetch';
 export { default as history } from './history';
 export { default as serverError } from './server-error';
