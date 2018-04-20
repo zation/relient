2ac08442e7d3903f4be056291b6e792d98bf67a6
Liu Yang <liuyang@waijule.com>
2018-04-20 15:56:33 +0800

    export all from validators

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,4 +3,4 @@ export { default as createOnSubmit } from './create-on-submit';
 export { default as i18n } from './i18n';
 export { default as getOptions } from './get-options';
 export { default as getText } from './get-text';
-export { default as validators } from './validators';
+export * as validators from './validators';
