c10501d384c951ff059a29d8780bdab46e18060f
Liu Yang <liuyang@waijule.com>
2018-04-23 12:40:01 +0800

    use isFinite instead of isNumber

--- src/formatters.js
+++ src/formatters.js
@@ -1,12 +1,12 @@
 import format from 'date-fns/fp/format';
-import { isNumber, trim, isNaN } from 'lodash/fp';
+import { isFinite, trim, isNaN } from 'lodash/fp';
 
 export const date = (formatter = 'YYYY-MM-DD') => format(formatter);
 
 export const time = (formatter = 'YYYY-MM-DD HH:mm:ss') => format(formatter);
 
 export const price = ({ currency = '￥', digit = 2, defaultDisplay = '--' } = {}) => (value) => {
-  if (isNumber(value)) {
+  if (isFinite(value)) {
     const number = parseFloat(Math.round(value * 100) / 100).toFixed(digit);
     return trim(`${currency} ${number}`);
   }
@@ -14,7 +14,7 @@ export const price = ({ currency = '￥', digit = 2, defaultDisplay = '--' } = {
 };
 
 export const percentage = ({ digit = 2, symbol = '%', defaultDisplay = '--' } = {}) => (value) => {
-  if (isNumber(value)) {
+  if (isFinite(value)) {
     const digits = 10 ** digit;
     const result = Math.round(Number(value) * digits * 100) / digits;
     return isNaN(result) ? defaultDisplay : `${result.toFixed(digit)}${symbol}`;
