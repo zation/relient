15897fac872cdbf6c09b8ca633cbe52e87a37e59
Zation <zation1@gmail.com>
2019-07-16 11:31:51 +0800

    improve validators

--- src/form.js
+++ src/form.js
@@ -20,17 +20,34 @@ export const requiredValidator = message => (value) => {
   return undefined;
 };
 
-export const sameAsValidator = message => name => (value, allValues) =>
-  (value === allValues[name] ? undefined : message);
+export const sameAsValidator = message => name => (value, allValues) => (
+  value === allValues[name] ? undefined : message
+);
 
-export const minLengthValidator = message => length => value =>
-  (value && value.length < length ? message : undefined);
+export const minLengthValidator = message => length => value => (
+  value && value.length < length ? message : undefined
+);
 
-export const maxLengthValidator = message => length => value =>
-  (value && value.length < length ? message : undefined);
+export const maxLengthValidator = message => length => value => (
+  value && value.length > length ? message : undefined
+);
 
-export const positiveNumberValidator = message => value =>
-  (value !== '' && Number(value) <= 0 ? message : undefined);
+export const positiveNumberValidator = message => value => (
+  value !== '' && Number(value) <= 0 ? message : undefined
+);
 
-export const lessThanValidator = message => name => (value, allValues) =>
-  (value !== '' && allValues[name] !== '' && Number(value) > Number(allValues[name]) ? message : undefined);
+export const lessOrEqualValidator = message => number => value => (
+  value !== '' && Number(value) <= number ? message : undefined
+);
+
+export const moreOrEqualValidator = message => number => value => (
+  value !== '' && Number(value) >= number ? message : undefined
+);
+
+export const lessOrEqualThanValidator = message => name => (value, allValues) => (
+  value !== '' && allValues[name] !== '' && Number(value) <= Number(allValues[name]) ? message : undefined
+);
+
+export const moreOrEqualThanValidator = message => name => (value, allValues) => (
+  value !== '' && allValues[name] !== '' && Number(value) >= Number(allValues[name]) ? message : undefined
+);
