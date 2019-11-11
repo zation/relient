f34f83a6ca2a471c2628aaac564c7463201ccf40
Zation <zation1@gmail.com>
2019-11-11 10:56:08 +0800

    fix i18n element key

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,8 @@ import { cloneElement, isValidElement } from 'react';
 import { parse, isLiteralElement, isArgumentElement } from 'intl-messageformat-parser';
 import { flow, map, prop, every, values as getValues, join, identity } from 'lodash/fp';
 
+const mapWithIndex = map.convert({ cap: false });
+
 export default (messages) => (messageKey, values) => {
   if (!messages) {
     throw new Error('Messages is required');
@@ -15,7 +17,7 @@ export default (messages) => (messageKey, values) => {
     return flow(
       prop(messageKey),
       parse,
-      map((element) => {
+      mapWithIndex((element, index) => {
         const { value, type } = element;
         if (isLiteralElement(element)) {
           return value;
@@ -23,7 +25,7 @@ export default (messages) => (messageKey, values) => {
         if (isArgumentElement(element)) {
           const argumentValue = values[value];
           return isValidElement(argumentValue)
-            ? cloneElement(argumentValue, { key: value }) : argumentValue;
+            ? cloneElement(argumentValue, { key: `${value}${index}` }) : argumentValue;
         }
         throw new Error(`Element type is not handled for: ${type}`);
       }),
