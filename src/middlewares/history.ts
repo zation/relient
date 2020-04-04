16780609364b3998ed10111f8f59a631217c6101
Zation <zation1@gmail.com>
2020-04-04 17:01:56 +0800

    handle baseUrl in history middleware

--- src/middlewares/history.ts
+++ src/middlewares/history.ts
@@ -1,16 +1,28 @@
 // TODO: remove below when ts 3.8 is supported by typescript-eslint: https://github.com/typescript-eslint/typescript-eslint/issues/1436
 // eslint-disable-next-line import/no-unresolved
 import type { History } from 'history';
+import { startsWith } from 'lodash/fp';
 import {
   PUSH, REPLACE, GO, GO_FORWARD, GO_BACK,
 } from '../actions/history';
+import getConfig from '../config';
+
+const baseUrl = getConfig('baseUrl');
 
 export default (history: History) => () => (next) => (action) => {
   const { payload, type } = action;
 
   if (type === PUSH) {
     const [pathname, hash] = payload.split('#');
-    history.push({ pathname, hash });
+    if (startsWith(baseUrl)(pathname)) {
+      history.push({ pathname, hash });
+    } else {
+      history.push({
+        pathname: `${baseUrl}/${pathname}`.replace(/([^:]\/)\/+/g, '$1'),
+        hash,
+      });
+    }
+
     return next({ ...action, payload: history.location });
   }
   if (type === REPLACE) {
