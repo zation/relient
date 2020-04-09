ae661608d431cc4c7b8231737f4ef8cc44e7912b
Zation <zation1@gmail.com>
2020-04-09 17:24:34 +0800

    improve get text

--- src/get-text.ts
+++ src/get-text.ts
@@ -1,5 +1,4 @@
 import {
-  flow,
   identity,
   propertyOf,
 } from 'lodash/fp';
@@ -7,4 +6,15 @@ import {
 export default (textMap: {
   [text: string]: any
   // @ts-ignore
-}) => (i18n = identity) => flow(propertyOf(textMap), i18n);
+}) => (i18n = identity) => (key) => {
+  if (key) {
+    // @ts-ignore
+    const text = propertyOf(textMap)(key);
+    if (text) {
+      return i18n(text);
+    }
+    console.warn(`Can not find proper key: ${key} in textMap: ${JSON.stringify(textMap)}`);
+  }
+
+  return '';
+};
