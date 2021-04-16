8cde92a26200f4aeb2090069f72029e69ccbfe21
Liu Yang <zation1@gmail.com>
2021-04-16 16:27:01 +0800

    change text to label in option for antd

--- src/get-options.ts
+++ src/get-options.ts
@@ -5,7 +5,7 @@ const mapWithKeys = map.convert({ cap: false });
 
 export default (textMap: {
   [text: string]: any
-}) => (i18n = identity) => mapWithKeys((text: string, key: string) => ({
+}) => (i18n = identity) => mapWithKeys((label: string, key: string) => ({
   value: key,
-  text: i18n(text),
+  label: i18n(label),
 }))(textMap);
